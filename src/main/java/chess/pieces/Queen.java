203fb9d9c8c4bafa13ae4841abf092878d606009
Jean Pereira Moreira - JPR <devjean96@gmail.com>
2022-01-27 23:32:00 -0300

    criacao da rainha

---
+++ src/main/java/chess/pieces/Queen.java
@@ -0,0 +1,100 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position auxPosi = new Position(0, 0);
+
+		// cima
+		auxPosi.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setRow(auxPosi.getRow() - 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		// esquerda
+		auxPosi.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setColumn(auxPosi.getColumn() - 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		// direita
+		auxPosi.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setColumn(auxPosi.getColumn() + 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		// baixo
+		auxPosi.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setRow(auxPosi.getRow() + 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		// noroeste
+		auxPosi.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setValues(auxPosi.getRow() - 1, auxPosi.getColumn() - 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		// nordeste
+		auxPosi.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setValues(auxPosi.getRow() - 1, auxPosi.getColumn() + 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		// sudeste
+		auxPosi.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setValues(auxPosi.getRow() + 1, auxPosi.getColumn() + 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		// sudoeste
+		auxPosi.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while (getBoard().positionExists(auxPosi) && !getBoard().thereIsAPiece(auxPosi)) {
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+			auxPosi.setValues(auxPosi.getRow() + 1, auxPosi.getColumn() - 1);
+		}
+		if (getBoard().positionExists(auxPosi) && isThereOpponentPiece(auxPosi))
+			matriz[auxPosi.getRow()][auxPosi.getColumn()] = true;
+
+		return matriz;
+	}
+
+}
